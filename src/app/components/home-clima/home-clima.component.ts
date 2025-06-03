796a934b111c92bc6f9daeefbc5aff9efce6dd96
MauroPolizzi <mauropolizzi2@gmail.com>
2025-06-03 16:51:18 -0300

    Create home component + add logic + pass information from home to widget-info-general component

--- src/app/components/home-clima/home-clima.component.ts
+++ src/app/components/home-clima/home-clima.component.ts
@@ -1,30 +1,36 @@
+import { IonRefresher, IonRefresherContent } from '@ionic/angular/standalone';
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { map, Observable } from 'rxjs';
+import { ClimaService } from 'src/app/services/clima.service';
 import { WidgetClimaComponent } from '../widget-clima/widget-clima.component';
 import { WidgetInfoGeneralComponent } from '../widget-info-general/widget-info-general.component';
 import { WidgetWeeklyForecastComponent } from '../widget-weekly-forecast/widget-weekly-forecast.component';
-import { IonRefresher, IonRefresherContent } from '@ionic/angular/standalone';
-import { ClimaService } from 'src/app/services/clima.service';
 import { IRootObject_GetForCityAndContry } from 'src/app/interfaces/IClimaApp.interfaces';
-import { map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home-clima',
   templateUrl: './home-clima.component.html',
   styleUrls: ['./home-clima.component.scss'],
-  imports: [WidgetClimaComponent, WidgetInfoGeneralComponent, WidgetWeeklyForecastComponent, IonRefresher, IonRefresherContent]
+  imports: [CommonModule, WidgetClimaComponent, WidgetInfoGeneralComponent, WidgetWeeklyForecastComponent, IonRefresher, IonRefresherContent]
 })
 export class HomeClimaComponent  implements OnInit {
 
-  public infoForCityAndContry: any;
+  public infoForCityAndContry: Observable<{
+    temperature: number;
+    temperaturaFormat: string;
+    temperatura_description: string;
+    temperatura_min: number;
+    temperatura_max: number;
+    city: string;
+  }>;
 
   constructor(private service: ClimaService) { 
     
-    this.infoForCityAndContry = this.getCurrentInfoForCityAndContryAsync().subscribe();
+    this.infoForCityAndContry = this.getCurrentInfoForCityAndContryAsync();
   }
   
-  ngOnInit() { 
-    //this.infoForCityAndContry = this.getCurrentInfoForCityAndContry();
-  }
+  ngOnInit() { }
 
   public getCurrentInfoForCityAndContryAsync(): Observable<{
     temperature: number;
@@ -37,10 +43,10 @@ export class HomeClimaComponent  implements OnInit {
     return this.service.getTemperatureStandarOfDay().pipe(
       map((resp: IRootObject_GetForCityAndContry) => {
         const temperature = parseFloat(resp.main.temp.toFixed(0));
-        const temperaturaFormat = `${temperature}°C`;
+        const temperaturaFormat = `${temperature}°`;
         const temperatura_description = resp.weather[0].description;
-        const temperatura_min = resp.main.temp_min;
-        const temperatura_max = resp.main.temp_max;
+        const temperatura_min = parseFloat(resp.main.temp_min.toFixed(0));
+        const temperatura_max = parseFloat(resp.main.temp_max.toFixed(0));
         const city = resp.name;
 
         return {
@@ -57,7 +63,8 @@ export class HomeClimaComponent  implements OnInit {
   public handleRefresh(event: CustomEvent) {
     setTimeout(() => {
       // Any calls to load data go here
-      (event.target as HTMLIonRefresherElement).complete();
+      (this.infoForCityAndContry = this.getCurrentInfoForCityAndContryAsync()); 
+      console.log(this.infoForCityAndContry);
     }, 2000);
   }
 }
--- src/app/components/widget-info-general/widget-info-general.component.ts
+++ src/app/components/widget-info-general/widget-info-general.component.ts
@@ -1,6 +1,4 @@
-import { Component, input, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { IRootObject_GetForCityAndContry } from 'src/app/interfaces/IClimaApp.interfaces';
+import { Component, Input } from '@angular/core';
 import { ClimaService } from 'src/app/services/clima.service';
 
 @Component({
@@ -8,30 +6,17 @@ import { ClimaService } from 'src/app/services/clima.service';
   templateUrl: './widget-info-general.component.html',
   styleUrls: ['./widget-info-general.component.scss'],
 })
-export class WidgetInfoGeneralComponent  implements OnInit {
+export class WidgetInfoGeneralComponent  {
 
-  public info_GetForCityAndContry = input.required();
+  @Input() info_GetForCityAndContry!: {
+    temperature: number;
+    temperaturaFormat: string;
+    temperatura_description: string;
+    temperatura_min: number;
+    temperatura_max: number;
+    city: string;
+  };
 
-  public temperatura: number = 0;
-  public temperaturaFormat: string = '';
-  public city: string = '';
-  public temperatura_description: string = '';
-  public temperatura_min: number = 0;
-  public temperatura_max: number = 0;
-    
-    constructor( private service: ClimaService ) { 
-  
-      this.service.getTemperatureStandarOfDay().subscribe( resp => { 
-        this.temperatura = parseFloat(resp.main.temp.toFixed(0));
-        this.city = resp.name;
-        this.temperatura_description = resp.weather[0].description;
-        this.temperatura_min = parseFloat(resp.main.temp_min.toFixed(0));
-        this.temperatura_max = parseFloat(resp.main.temp_max.toFixed(0))
-        this.temperaturaFormat = `${parseFloat(this.temperatura.toFixed(0))}°`;
-       });
-    }
-
-  ngOnInit() {
-  }
+  constructor( private service: ClimaService ) { }
   
 }
