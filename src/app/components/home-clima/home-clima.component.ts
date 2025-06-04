9025c6e36078476eb0fc0ac5e58244a5f86328e3
MauroPolizzi <mauropolizzi2@gmail.com>
2025-06-04 18:10:13 -0300

    Fix the logic in home component + Update ion-refresher

--- src/app/components/home-clima/home-clima.component.ts
+++ src/app/components/home-clima/home-clima.component.ts
@@ -1,7 +1,7 @@
-import { IonRefresher, IonRefresherContent } from '@ionic/angular/standalone';
+import { IonContent, IonRefresher, IonRefresherContent, IonSpinner } from '@ionic/angular/standalone';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { map, Observable } from 'rxjs';
+import { finalize, forkJoin, map, Observable, timer } from 'rxjs';
 import { ClimaService } from 'src/app/services/clima.service';
 import { WidgetClimaComponent } from '../widget-clima/widget-clima.component';
 import { WidgetInfoGeneralComponent } from '../widget-info-general/widget-info-general.component';
@@ -12,59 +12,57 @@ import { IRootObject_GetForCityAndContry } from 'src/app/interfaces/IClimaApp.in
   selector: 'app-home-clima',
   templateUrl: './home-clima.component.html',
   styleUrls: ['./home-clima.component.scss'],
-  imports: [CommonModule, WidgetClimaComponent, WidgetInfoGeneralComponent, WidgetWeeklyForecastComponent, IonRefresher, IonRefresherContent]
+  imports: [CommonModule, WidgetClimaComponent, WidgetInfoGeneralComponent, WidgetWeeklyForecastComponent, IonContent, IonRefresher, IonRefresherContent, IonSpinner]
 })
 export class HomeClimaComponent  implements OnInit {
 
-  public infoForCityAndContry: Observable<{
+  public loading: boolean = true;
+  public weatherData!: {
     temperature: number;
     temperaturaFormat: string;
     temperatura_description: string;
     temperatura_min: number;
     temperatura_max: number;
     city: string;
-  }>;
+  };
 
-  constructor(private service: ClimaService) { 
-    
-    this.infoForCityAndContry = this.getCurrentInfoForCityAndContryAsync();
-  }
+  constructor(private service: ClimaService) { }
   
-  ngOnInit() { }
+  ngOnInit() { this.loadWeatherData() }
 
-  public getCurrentInfoForCityAndContryAsync(): Observable<{
-    temperature: number;
-    temperaturaFormat: string;
-    temperatura_description: string;
-    temperatura_min: number;
-    temperatura_max: number;
-    city: string;
-  }> {
-    return this.service.getTemperatureStandarOfDay().pipe(
-      map((resp: IRootObject_GetForCityAndContry) => {
-        const temperature = parseFloat(resp.main.temp.toFixed(0));
-        const temperaturaFormat = `${temperature}°`;
-        const temperatura_description = resp.weather[0].description;
-        const temperatura_min = parseFloat(resp.main.temp_min.toFixed(0));
-        const temperatura_max = parseFloat(resp.main.temp_max.toFixed(0));
-        const city = resp.name;
+  public loadWeatherData(event?: any): void {
+    if (!event) this.loading = true;
 
-        return {
-          temperature,
-          temperaturaFormat,
-          temperatura_description,
-          temperatura_min,
-          temperatura_max,
-          city
-        };
-    }));
-  }
-
-  public handleRefresh(event: CustomEvent) {
-    setTimeout(() => {
-      // Any calls to load data go here
-      (this.infoForCityAndContry = this.getCurrentInfoForCityAndContryAsync()); 
-      console.log(this.infoForCityAndContry);
-    }, 2000);
+    const weather: Observable<any> = this.service.getTemperatureStandarOfDay()
+      .pipe(
+        map((resp: IRootObject_GetForCityAndContry) => ({
+          temperature: parseFloat(resp.main.temp.toFixed(0)),
+          temperaturaFormat: `${parseFloat(resp.main.temp.toFixed(0))}°`,
+          temperatura_description: resp.weather[0].description,
+          temperatura_min: parseFloat(resp.main.temp_min.toFixed(0)),
+          temperatura_max: parseFloat(resp.main.temp_max.toFixed(0)),
+          city: resp.name,
+        })),
+      )
+      
+      // Establecemos el tiempo de espera
+      const delay = timer(1500);
+      
+      forkJoin([weather, delay])
+        .pipe(
+          finalize(() => {
+            this.loading = false;
+            if (event) {
+              event.target.complete(); // Finaliza la animación de refresco
+            }
+            this.loading = false;
+          })
+        )
+      .subscribe({
+        next: ([data]) => this.weatherData = data,
+        error: (err) => {
+          console.error('Error al cargar datos del clima', err);
+        }
+    });
   }
 }
