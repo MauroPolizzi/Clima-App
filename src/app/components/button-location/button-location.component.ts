cb66301dbfe461525a14663a8ff8b47e5c8a454e
MauroPolizzi <mauropolizzi2@gmail.com>
2025-07-22 16:18:11 -0300

    Fix in component location

--- src/app/components/button-location/button-location.component.ts
+++ src/app/components/button-location/button-location.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { IonContent, IonFab, IonFabButton, IonFabList, IonIcon, IonSearchbar } from '@ionic/angular/standalone'
+import { NgFor, NgIf } from '@angular/common';
+import { IonSearchbar, IonList, IonItem, IonLabel } from '@ionic/angular/standalone'
+import { CountriesService } from '../../services/countries.service';
+import { ICountry_CountryStateCity, ICountryDetail_RestCountry } from 'src/app/interfaces/IClimaApp.interfaces';
 
 @Component({
   selector: 'app-button-location',
   templateUrl: './button-location.component.html',
   styleUrls: ['./button-location.component.scss'],
-  imports: [IonContent, IonFab, IonFabButton, IonFabList, IonIcon, IonSearchbar]
+  imports: [IonSearchbar, IonList, IonItem, IonLabel, NgIf, NgFor]
 })
 export class ButtonLocationComponent  implements OnInit {
 
-  constructor() { }
+  public countryList: ICountryDetail_RestCountry[] = [];
+
+  constructor(private countriesService : CountriesService) { }
 
   ngOnInit() {}
+
+  onSearchChange(event: any) {
+    let search: string = event.detail.value;
+    if(search.length > 3) {
+      this.countriesService.getCountryByNameAPiRestCountry(search)
+        .subscribe((resp: ICountryDetail_RestCountry[]) => {
+          this.countryList = resp;
+          console.log(this.countryList);
+        });
+    }
+  }
 }
