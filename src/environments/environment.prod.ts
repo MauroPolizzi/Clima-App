259e81a6fe1f24bd03294937a79c29a2f66d3105
MauroPolizzi <mauropolizzi2@gmail.com>
2025-07-23 14:33:57 -0300

    Add environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,33 @@
 export const environment = {
-  production: true
+  production: true,
+
+   //#region API Open Weather Map
+  /** End Point que devuelve datos basicos del pronostico para el dia actual, segun los parametros de q (nombre de ciudad separado por coma codigo ISO del pais). */
+  //'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=' END POINT General,
+  // Aqui apuntamos directamente a San Miguel de Tucuman
+  API_ENDPOINT_GETFORCITYANDCOUNTRY: 'https://api.openweathermap.org/data/2.5/weather?lat=-26.82414000&lon=-65.22260000&appid=', 
+  
+  /** End Point para que devuelve una lista de pronosticos para las siguientes 5 dias cada 3 horas 
+   * Con el params cnt definimos el limite de objetos dentro de la lista devuelta.
+  */
+  //'https://pro.openweathermap.org/data/2.5/forecast?q=London&cnt=40&appid=' END POINT General
+ // Aqui apuntamos directamente a San Miguel de Tucuman
+  API_ENDPOINT_GETWEEKLYFORECAST: 'https://pro.openweathermap.org/data/2.5/forecast?lat=-26.82414000&lon=-65.22260000&cnt=40&appid=', 
+  API_KEY: '974a07281e5b6d25f5d9c7a55caa9743',
+
+  URL_ICON: 'https://openweathermap.org/img/wn/',
+  //#endregion
+
+  //#region API de Rest Countries
+  /** End Point que devuelve todos los paises con su nombre (name), capital (capital) y bandera (flags). */
+  API_RESTCOUNTRY_ENDPOINT_ALL_COUNTRIES: 'https://restcountries.com/v3.1/all?fields=name,capital,flags',
+
+  /** End Point que devuelve los paises coincidentes con el path params {name} (https://restcountries.com/v3.1/name/{name}). */
+  API_RESTCOUNTRY_ENDPOINT_NAME_COUNTRY: 'https://restcountries.com/v3.1/name/',
+
+  /** End Point que devuelve el paise, segun su capital proporcionada con el path params {capital} (https://restcountries.com/v3.1/capital/{capital}). */
+  API_RESTCOUNTRY_ENDPOINT_CAPITAL_COUNTRY: 'https://restcountries.com/v3.1/capital/',
+//#endregion
+
+  //#region API de Country State City
 };
